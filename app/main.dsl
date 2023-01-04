9a7a877463046ea264df63d6a9c9d72c641de2bb
Softworth Solutions Private Limited <79741172+harikrishnan-sws@users.noreply.github.com>
2023-01-05 02:03:06 +0530

    implemented first dasha app

--- app/main.dsl
+++ app/main.dsl
@@ -1,18 +1,18 @@
 context 
 {
     // declare input variables here
-    input phone: string
+    input phone: string;
 }
 
 start node root {
     do
     {
-        #connectSafe($phone) 
+        #connectSafe($phone);
         // connecting to the phone number which we receive as input from the node app
-        #waitForSpeech(1000)
+        #waitForSpeech(1000);
         // give the person one second to start speaking
-        #say("greeting")
+        #say("greeting");
         // greet them. This phrase is obtained from the phraseMap.json
-        wait *.;
+        wait *;
     }
 }
--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-function main() {
-    console.log("my dasha app");
+const dasha = require("@dasha.ai/sdk");
+
+async function main() {
+  const app = await dasha.deploy("./app");
+
+  await app.start();
+
+  const conv = app.createConversation({
+    phone: process.argv[2] ?? "",
+  });
+
+  conv.audio.tts = "dasha";
+
+  conv.on("transcription", console.log);
+
+  const result = await conv.execute({
+    channel: "audio",
+  });
+
+  console.log(result.output);
+
+  await app.stop();
+
+  await app.dispose();
 }
 
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+})
